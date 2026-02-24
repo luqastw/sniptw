9a45c1f682112da7f2d2b173b7da811cda120ab5
luqastw <lucasborcem1@gmail.com>
2026-02-24 13:11:29 -0300

    feat: initial setup and config

---
+++ backend/app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    REDIS_URL: str = "redis://localhost:6379"
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    BASE_URL: str = "http://localhost:8000"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+
